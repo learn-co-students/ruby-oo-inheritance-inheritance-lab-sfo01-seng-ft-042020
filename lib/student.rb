41f928fb5a091f7de2b5bcc8b3a0df6099ccfe23
finalbout <johntjr.22@gmail.com>
2020-04-27 09:30:13 -0700

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,22 @@
-class Student 
+class Student < User
+
+    attr_accessor :knowledge
+
+
+def initialize
+    @knowledge = []
+end
+
+def learn(str)
+    knowledge << str
+
+
+    #sophie.learn("new code")
+end
+
+
 
 end
+
+#sophie.instance_varable_get => @knowledge
+#sophie.new => []
